607a91fc98ad1347b55f10ac8ab335b7f3965ab9
wallace <wallace@ime.eb.br>
2018-10-18 15:53:38 -0300

    Started socket io

--- server/server.js
+++ server/server.js
@@ -1,17 +1,21 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 var app = express(); // Init Express
+var server = http.createServer(app); // Creating the server
+var io = socketIO(server);
 
 // Add this folder to be loaded. 
 // If go localhost:3000/help.html, the /public/help.html will be loaded without type /public/ at URL.
 app.use(express.static(publicPath));
 
-// Starting Express
-app.listen(port, () => {
+// Starting server
+server.listen(port, () => {
 	console.log(`Server is up at port ${port}`);
 })
 
